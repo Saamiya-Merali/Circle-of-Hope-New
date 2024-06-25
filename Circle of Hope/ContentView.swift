642cc0fe8a7ed21519d54e0d58f3b4b7ce65b01a
Isabella Gomez Rodriguez <isabellagomezr@gmail.com>
2024-06-25 13:42:50 -0400

    Isabella's changes

--- Circle of Hope/ContentView.swift
+++ Circle of Hope/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello team 5! welcome to new project")
+            Text("Isabella was here")
         }
         .padding()
     }
