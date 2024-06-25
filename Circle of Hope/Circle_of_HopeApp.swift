7ec81185105a2cc779adaf7c0e4c6dd129919293
Saamiya-Merali <Saamiyamerali@gmail.com>
2024-06-25 12:35:47 -0500

    Initial Commit

---
+++ Circle of Hope/Circle_of_HopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circle_of_HopeApp.swift
+//  Circle of Hope
+//
+//  Created by Isabella Gomez Rodriguez on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Circle_of_HopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circle of Hope/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Circle of Hope
+//
+//  Created by Isabella Gomez Rodriguez on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
