f3f0bce11b32fbd3e96016a69d6bbf347c1406dc
Saamiya-Merali <Saamiyamerali@gmail.com>
2024-06-25 12:38:28 -0500

    welcome message added

--- Circle of Hope/ContentView.swift
+++ Circle of Hope/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello")
+            Text("Hello team 5! welcome to new project")
         }
         .padding()
     }
