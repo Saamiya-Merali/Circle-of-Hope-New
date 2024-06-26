f525fa28f2dcc02f0bb194048439d6b801884d8d
Isabella Gomez Rodriguez <isabellagomezr@gmail.com>
2024-06-26 10:20:36 -0400

    Set up the main screen and added two files

---
+++ Circle of Hope/CharityPage.swift
@@ -0,0 +1,18 @@
+//
+//  CharityPage.swift
+//  Circle of Hope
+//
+//  Created by Isabella Gomez Rodriguez on 6/26/24.
+//
+
+import SwiftUI
+
+struct CharityPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharityPage()
+}
