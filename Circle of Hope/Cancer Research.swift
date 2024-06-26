935878736fde50342cbe2c6b3fe33ae968a61ad4
Saamiya-Merali <Saamiyamerali@gmail.com>
2024-06-26 10:28:17 -0500

    Cancer Research Page

---
+++ Circle of Hope/Cancer Research.swift
@@ -0,0 +1,18 @@
+//
+//  Cancer Research.swift
+//  Circle of Hope
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+struct Cancer_Research: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cancer_Research()
+}
