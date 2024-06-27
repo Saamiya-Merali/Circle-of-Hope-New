3f8718151c4f3fb9126d7e6a028458506f2c3231
Isabella Gomez Rodriguez <isabellagomezr@gmail.com>
2024-06-27 10:40:20 -0400

    Added quiz questions as files, added in the charity page

---
+++ Circle of Hope/QuizQuestionTwo.swift
@@ -0,0 +1,18 @@
+//
+//  QuizQuestionTwo.swift
+//  Circle of Hope
+//
+//  Created by Isabella Gomez Rodriguez on 6/27/24.
+//
+
+import SwiftUI
+
+struct QuizQuestionTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizQuestionTwo()
+}
